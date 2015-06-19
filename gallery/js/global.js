2fa9242107ac51c8a72d692038e23108316f3bd4
niallcalvert <niallcalvert@google.com>
2015-06-19 17:07:26 +0100

    Post Request Test

--- gallery/js/global.js
+++ gallery/js/global.js
@@ -1,6 +1,9 @@
 // URLS
 var getUrl = 'http://146.148.2.249:3000/artworks/artwork/';
+var postUrl = 'http://146.148.2.249/artworks/updateShareCounter/'
 var imgUrl = 'http://146.148.2.249/';
+var imgParam;
+var postResponse;
 
 
 // Handle click on share icons.
@@ -10,15 +13,18 @@ function shareOn() {
   var currentHref = location.href;
   switch (name) {
     case 'fa fa-google-plus':
+      postRequest(postUrl + 'googleplus/:' + imgParam);
       element.href = 'https://plus.google.com/share?url=' +
           currentHref;
       break;
     case 'fa fa-facebook':
+      postRequest(postUrl + 'facebookplus/:' + imgParam);
       element.href =
           'http://www.facebook.com/sharer.php?t=Example text' +
           '&u=' + currentHref;
       break;
     case 'fa fa-twitter':
+      postRequest(postUrl + 'twitterplus/:' + imgParam);
       element.href = 'https://twitter.com/intent/tweet?text=' +
           'Example text @GOOGLECANNES ' +
           currentHref;
@@ -32,34 +38,32 @@ function getRequest(url) {
   var myRequest = new XMLHttpRequest();
   myRequest.open('get', url, false);
   myRequest.send();
-  return myRequest.responseText;          
+  return myRequest.responseText;
 }
 
 
 // Handle POST request
-function postRequest() {
+function postRequest(url) {
   var myRequest = new XMLHttpRequest();
+  myRequest.open('POST', url, true);
+  myRequest.send();
 }
 
 
-// Get Param(id) from url, then get the image using the param.
 // Add image to DOM.
-function getImage() {
-  var imgParam = location.href.split('id=')[1];
-  var createUrl = getUrl + imgParam;
-  var json_obj = JSON.parse(getRequest(createUrl));
-  var myImage = imgUrl + json_obj.imageURL;
+function displayImage() {
+  var myImage = imgUrl + postResponse.imageURL;
   var img = document.createElement('IMG');
   img.setAttribute('src', myImage);
   img.setAttribute('class', 'my-image');
   document.getElementById('container').appendChild(img);
-  // console.log(img);
-  // var element = document.getElementsByTagName('BODY');
-  // element[0].style.backgroundImage = 'url(' + myImage + ')';
 }
 
 
 window.addEventListener('load', function() {
+  imgParam = location.href.split('id=')[1];
+  var createUrl = getUrl + imgParam;
+  postResponse = JSON.parse(getRequest(createUrl));
   document.addEventListener('click', shareOn);
-  getImage();
+  displayImage();
 });
