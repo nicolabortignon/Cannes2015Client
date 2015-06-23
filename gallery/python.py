4486ab8575fbecf3d7bf018eceb9ccfd42bd44c2
Nicola Bortignon <info@nicolabortignon.com>
2015-06-23 18:15:27 +0100

    pages

--- gallery/python.py
+++ gallery/python.py
@@ -1,7 +1,15 @@
-with open ("index.html", "r") as myfile:
-    data=myfile.read().replace('\n', '')
-    id = 9
-    newData = data.replace('%', ''+str(id))
-    text_file = open("index"+str(id)+".html", "w")
-    text_file.write(newData)
-    text_file.close()
+import csv
+
+with open("tabseparatedInfo") as tsv:
+    for line in csv.reader(tsv, dialect="excel-tab"):
+    	with open ("index.html", "r") as myfile:
+		    data=myfile.read().replace('\n', '')
+		    id = line[0]
+		    newData = data.replace('%', ''+str(id))
+		    newData = newData.replace('*', ''+str(line[1]))
+		    text_file = open("index"+str(id)+".html", "w")
+		    text_file.write(newData)
+		    text_file.close()
+		 
+
+		
