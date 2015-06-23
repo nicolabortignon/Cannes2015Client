be20d6400206a60f29ca69ac6c1925716cda3dc7
Nicola Bortignon <info@nicolabortignon.com>
2015-06-23 17:24:03 +0100

    changed Gallery

--- gallery/js/global.js
+++ gallery/js/global.js
@@ -8,62 +8,61 @@ var postResponse;
 
 // Handle click on share icons.
 function shareOn(event) {
-  var name = event.target.className;
-  var element = event.target;
-  var currentHref = location.href;
-  switch (name) {
-    case 'fa fa-google-plus':
-      getRequest(postUrl + 'googleplus/' + imgParam);
-      element.href = 'https://plus.google.com/share?url=' +
-          postResponse.shortURL;
-      break;
-    case 'fa fa-facebook':
-      getRequest(postUrl + 'facebook/' + imgParam);
-      // element.href =
+    var name = event.target.className;
+    var element = event.target;
+    var currentHref = location.href;
+    switch (name) {
+        case 'fa fa-google-plus':
+            getRequest(postUrl + 'googleplus/' + imgParam);
+            element.href = 'https://plus.google.com/share?url=' +
+                postResponse.shortURL;
+            break;
+        case 'fa fa-facebook':
+            getRequest(postUrl + 'facebook/' + imgParam);
+            // element.href =
             // 'https://www.facebook.com/dialog/share?' +
             // 'app_id=650727728361999' +
             // '&display=popup' +
             // '&href=http://146.148.2.249/' +
             // '&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer';
-            FB.ui(
-             {
-              method: 'share',
-              href: 'http://146.148.2.249/gallery/index.html?id=9',
-            }, function(response){});
-      break;
-    case 'fa fa-twitter':
-      getRequest(postUrl + 'twitter/' + imgParam);
-      element.href = 'https://twitter.com/intent/tweet?text=' +
-          'Example text @GOOGLECANNES ' +
-          postResponse.shortURL + '&hashtags=hello,anotherone';
-      break;
-  }
+            FB.ui({
+                method: 'share',
+                href: 'http://146.148.2.249/gallery/index.html?id=9',
+            }, function(response) {});
+            break;
+        case 'fa fa-twitter':
+            getRequest(postUrl + 'twitter/' + imgParam);
+            element.href = 'https://twitter.com/intent/tweet?text=' +
+                'I love this street art DoubleClick recommended for me @DoubleClick ' +
+                postResponse.shortURL + '&hashtags=StreetArt,Cannes2015';
+            break;
+    }
 };
 
 
 // Handle GET request.
 function getRequest(url) {
-  var myRequest = new XMLHttpRequest();
-  myRequest.open('get', url, false);
-  myRequest.send();
-  return myRequest.responseText;
+    var myRequest = new XMLHttpRequest();
+    myRequest.open('get', url, false);
+    myRequest.send();
+    return myRequest.responseText;
 }
 
 
 // Add image to DOM.
 function displayImage() {
-  var myImage = imgUrl + postResponse.imageURL;
-  var img = document.createElement('IMG');
-  img.setAttribute('src', myImage);
-  img.setAttribute('class', 'my-image');
-  document.getElementById('container').appendChild(img);
+    var myImage = imgUrl + postResponse.imageURL;
+    var img = document.createElement('IMG');
+    img.setAttribute('src', myImage);
+    img.setAttribute('class', 'my-image');
+    document.getElementById('container').appendChild(img);
 }
 
 
 window.addEventListener('load', function() {
-  imgParam = location.href.split('id=')[1];
-  var createUrl = getUrl + imgParam;
-  postResponse = JSON.parse(getRequest(createUrl));
-  document.addEventListener('click', shareOn);
-  displayImage();
+    imgParam = location.href.split('id=')[1];
+    var createUrl = getUrl + imgParam;
+    postResponse = JSON.parse(getRequest(createUrl));
+    document.addEventListener('click', shareOn);
+    displayImage();
 });
